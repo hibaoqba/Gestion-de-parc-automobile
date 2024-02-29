7c7c0d70a636deb208de806f9c4f09f048148e19
GhizlaneMoudane <ghizmoudane@gmail.com>
2024-02-29 09:49:41 +0100

    Initial commit

---
+++ src/main/java/org/example/miniproj/MiniprojApplication.java
@@ -0,0 +1,13 @@
+package org.example.miniproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/miniproj/MiniprojApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.miniproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
