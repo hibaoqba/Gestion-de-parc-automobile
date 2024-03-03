52fb2d04bb9f4f4026c945d611542a99a791e2ff
GhizlaneMoudane <ghizmoudane@gmail.com>
2024-03-03 18:32:16 +0100

    driver and trip

---
+++ src/main/java/org/example/miniproj/model/Driver.java
@@ -0,0 +1,25 @@
+package org.example.miniproj.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="driver")
+public class Driver {
+    @Id
+    @GeneratedValue
+    private int matricule;
+
+
+
+}
---
+++ src/main/java/org/example/miniproj/model/Trip.java
@@ -0,0 +1,38 @@
+package org.example.miniproj.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table()
+public class Trip {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String departure;
+    private String destination;
+    private LocalDate departureDate;
+    private LocalDate arrivaleDate;
+    private LocalTime departureTime;
+    private LocalTime arrivaleTime;
+    private int numberOfPassengers;
+    @Enumerated(EnumType.STRING)
+    private VehiculeType vehiculeType;
+    @ManyToOne
+    @JoinColumn(name="matricule")
+    private Driver driver;
+
+}
