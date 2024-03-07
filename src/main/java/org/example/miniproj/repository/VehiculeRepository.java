006ab97e0c7a9054ab40714d406a6b4c6c20b751
hiba oqba <hibaoqba2003@gmail.com>
2024-03-07 09:38:42 +0100

    repositories

--- src/main/java/org/example/miniproj/repository/VehiculeRepository.java
+++ src/main/java/org/example/miniproj/repository/VehiculeRepository.java
@@ -2,7 +2,9 @@ package org.example.miniproj.repository;
 
 import org.example.miniproj.model.Vehicule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehiculeRepository extends JpaRepository<Vehicule,String> {
 
 }
