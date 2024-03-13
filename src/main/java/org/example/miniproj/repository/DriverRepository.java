e0b45eebfe6a2efb02897b1b80653c9a73fb6ae2
GhizlaneMoudane <ghizmoudane@gmail.com>
2024-03-13 19:36:29 +0100

    service driver filled in

--- src/main/java/org/example/miniproj/repository/DriverRepository.java
+++ src/main/java/org/example/miniproj/repository/DriverRepository.java
@@ -3,5 +3,9 @@ package org.example.miniproj.repository;
 import org.example.miniproj.model.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DriverRepository extends JpaRepository<Driver,String> {
+    Optional<Driver> findByMatricule(String matricule);
+
 }
--- src/main/java/org/example/miniproj/service/DriverServiceImpl.java
+++ src/main/java/org/example/miniproj/service/DriverServiceImpl.java
@@ -1,4 +1,43 @@
 package org.example.miniproj.service;
 
-public class DriverServiceImpl {
+import org.example.miniproj.model.Driver;
+import org.example.miniproj.repository.DriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DriverServiceImpl implements DriverService {
+
+    private DriverRepository driverRepository;
+
+    @Autowired
+    public DriverServiceImpl(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
+
+    @Override
+    public List<Driver> getAllDrivers() {
+        return driverRepository.findAll();
+    }
+
+    @Override
+    public Optional<Driver> getDriverById(String matricule) {
+        return driverRepository.findByMatricule(matricule);
+    }
+
+
+
+
+    @Override
+    public Driver saveDriver(Driver driver) {
+        return driverRepository.save(driver);
+    }
+
+    @Override
+    public void deleteDriver(String matricule) {
+        driverRepository.deleteByMatricule(matricule);
+    }
 }
